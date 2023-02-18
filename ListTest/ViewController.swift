1c235b4d124e3381f9d54b4f6418968776199542
soyombo <soyombo0@naver.com>
2023-02-18 04:54:25 +0300

    Initial Commit

---
+++ ListTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListTest
+//
+//  Created by Soyombo Mantaagiin on 18.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
